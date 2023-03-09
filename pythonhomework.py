fb3f21a4b110a0fe02426385072f4799646dbcd5
Mahmut İsa Babaoğlu <119530232+isa-babaoglu@users.noreply.github.com>
2023-03-09 13:30:09 +0300

    Add files via upload

---
+++ pythonhomework.py
@@ -0,0 +1,13 @@
+kg=120
+m=1.90
+kg/=m*m
+if 18.5 < kg and kg< 24.9 :
+    print("Normal")
+elif 25 < kg and kg < 29.9 :
+    print("Fazla kilolu")
+elif 30 < kg and kg < 34.9 :
+    print("I. derece obez")
+elif 35 < kg and kg < 39.9 :
+    print("II. derece obez")
+elif kg and kg > 40 :
+    print("III. derece obez")
